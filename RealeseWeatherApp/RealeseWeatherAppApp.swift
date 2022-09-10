b69d258e89f009ff7375bc7f4784f4438ae6f76c
hu_f <hu_f@live.com>
2022-09-10 12:49:04 +0300

    Initial Commit

---
+++ RealeseWeatherApp/RealeseWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealeseWeatherAppApp.swift
+//  RealeseWeatherApp
+//
+//  Created by huf on 10.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct RealeseWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
